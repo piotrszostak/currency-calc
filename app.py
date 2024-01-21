86efc4e52b704976db5ca5806715eb4101ed48c3
ps <pio.szo@gmial.com>
2024-01-21 15:09:07 +0100

    Add 'result' form

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ app = Flask(__name__)
 def calc():
     rates = fetch_rates()
     return render_template("index.html", rates=rates)
+
+@app.route('/result', methods=['POST'])
+def result():
+    result = request.form # -> MultiDict ([(currency, "USD"), (amount: 123)])
+    rates = fetch_rates()
+
+    currency = [currency_dict for currency_dict in rates if currency_dict['code'] == result["currency"]][0]
+    ask = currency['ask']
+    amount = int(result['amount'])
+    value = round(ask * amount, ndigits=2)
+    return render_template("result.html", value=value, ask=ask, amount=amount, currency=currency)
--- curr.py
+++ curr.py
@@ -1,23 +1,26 @@
 import requests
 import csv
 
-# get json and convert to python list of dicts
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
-
-# convert list into a dict and extract 'rates' value (a list)
-dct = {}
-for i in data:
-    dct.update(i)
-
-rates_list = dct['rates']
+def fetch_rates():
+    # get json and convert to python list of dicts
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+    data = response.json()
+    
+    # convert list into a dict and extract 'rates' value (a list)
+    dct = {}
+    for i in data:
+        dct.update(i)
+    return dct['rates']
 
 def list_to_csv(data_list, csv_filename):
     with open(csv_filename, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=data_list[0].keys(), delimiter=';')
         
         writer.writeheader()
-        writer.writerows(data_list)
+        # [:-1] to trim last line
+        writer.writerows(data_list[:-1])
 
+
+rates_list = fetch_rates()
 csv_file = 'output.csv'
 list_to_csv(rates_list, csv_file)
